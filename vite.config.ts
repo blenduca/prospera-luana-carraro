503b7fa75f0a9e100e3de871920f42266629d479
Matheus Chagas Dias <matheuscd10@gmail.com>
2026-06-16 16:52:39 -0300

    vercel build fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/luana-funil-evento/',
   plugins: [
     react(),
     tailwindcss(),
